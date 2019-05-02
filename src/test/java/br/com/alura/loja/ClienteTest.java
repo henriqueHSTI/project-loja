55daaf9a8fef89dfbb6aa433f9fa308cec099868
Jose Geraldo Henrique da Silva <henrique@hsti.eng.br>
2019-05-02 19:01:07 -0300

    UPLOAD CODE

---
+++ src/test/java/br/com/alura/loja/ClienteTest.java
@@ -0,0 +1,74 @@
+package br.com.alura.loja;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.filter.LoggingFilter;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.thoughtworks.xstream.XStream;
+
+import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.modelo.Produto;
+import br.com.alura.loja.modelo.Projeto;
+
+public class ClienteTest {
+	private HttpServer server;
+
+	@Before
+	public void startdesServer() {
+		server = Servidor.initializeServer();
+	}
+
+	@After
+	public void finalizedServer() {
+		server.stop();
+	}
+
+	@Test
+	public void testaQueAConexaoComOServidorFunciona() {
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://localhost:8080");
+		String conteudo = target.path("/carrinhos/1").request().get(String.class);
+		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
+		Assert.assertEquals("Rua Vergueiro 3185, 8 andar", carrinho.getRua());
+	}
+
+	@Test
+	public void testProjetoResource() {
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://localhost:8080");
+		String conteudo = target.path("/projetos/1").request().get(String.class);
+		Projeto projeto = (Projeto) new XStream().fromXML(conteudo);
+		Assert.assertEquals("Minha loja", projeto.getNome());
+	}
+
+	@Test
+	public void testInsertCarrinho() {
+		ClientConfig config = new ClientConfig();
+		config.register(new LoggingFilter());
+		Client client = ClientBuilder.newClient(config);
+		WebTarget target = client.target("http://localhost:8080");
+		Carrinho carrinho = new Carrinho();
+		carrinho.adiciona(new Produto(314L, "Tablet", 999, 1));
+		carrinho.setRua("Rua Vergueiro");
+		carrinho.setCidade("Sao Paulo");
+		String xml = carrinho.toXml();
+		Entity<String> entity = Entity.entity(xml, MediaType.APPLICATION_XML);
+		Response response = target.path("/carrinhos").request().post(entity);
+		Assert.assertEquals(201, response.getStatus());
+		String location = response.getHeaderString("Location");
+		Carrinho carrinho2 = client.target(location).request().get(Carrinho.class);
+		Assert.assertTrue(carrinho2.getProdutos().get(0).getNome().contains("Microfone"));
+	}
+
+}
---
+++ src/test/java/br/com/alura/loja/Servidor.java
@@ -0,0 +1,24 @@
+package br.com.alura.loja;
+
+import java.io.IOException;
+import java.net.URI;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+public class Servidor {
+	public static void main(String[] args) throws IOException {
+		HttpServer server = initializeServer();
+		System.in.read();
+		server.stop();
+	}
+
+	public static HttpServer initializeServer() {
+		URI uri = URI.create("http://localhost:8080/");
+		ResourceConfig config = new ResourceConfig().packages("br.com.alura.loja");
+		HttpServer server = GrizzlyHttpServerFactory.createHttpServer(uri, config);
+		return server;
+	}
+
+}
